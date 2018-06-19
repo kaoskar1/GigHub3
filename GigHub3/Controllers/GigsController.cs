074f41e6cd760b1d00a964c95d6b67e3d1aa4c2e
Oscar Ljungdahl <oscar.ljungdahl@hotmail.com>
2018-06-19 10:48:32 +0200

    form tabel

--- GigHub3/Controllers/GigsController.cs
+++ GigHub3/Controllers/GigsController.cs
@@ -3,20 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GigHub3.Models;
+using GigHub3.ViewModels;
 
 namespace GigHub3.Controllers
 {
     public class GigsController : Controller
     {
-        // GET: Gigs
-        public ActionResult Index()
+
+
+        private ApplicationDbContext _context;
+
+        public GigsController()
         {
-            return View();
+            _context = new ApplicationDbContext();
         }
+        // GET: Gigs
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
 
         public ActionResult Create()
         {
-           return View();
+            var ViewModel = new GigFormViewModel
+            {
+
+                Genres = _context.Genres.ToList()
+
+            };
+
+           return View(ViewModel);
         }
     }
 }
